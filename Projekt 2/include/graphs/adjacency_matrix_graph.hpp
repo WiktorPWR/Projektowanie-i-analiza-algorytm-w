88000a83ef7a7f454d03325d3f881384b0e9bfa1
Wiktor Jankowski Ostrowski <122821918+WiktorPWR@users.noreply.github.com>
2024-04-17 23:41:03 +0200

    virtual

--- Projekt 2/include/graphs/adjacency_matrix_graph.hpp
+++ Projekt 2/include/graphs/adjacency_matrix_graph.hpp
@@ -2,14 +2,25 @@
 #define ADJACENCY_MATRIX_GRAPH_HPP_
 
 #include <memory>
-
+#include <vector>
 #include "graphs/graph.hpp"
+template <typename T>
 
 class AdjacencyMatrixGraph : public Graph
 {
-
+  private:
+    std::vector<std::vector<T>> matrix;
+    
   public:
     // TODO: implement
+    void add_vertex(T vectex)
+    {
+      matrix.insert(matrix.begin()+ vectex,0);
+    }
+    void add_edge(T vectex_1, T vectex_2)
+    {
+      matrix.insert(matrix.begin()+vectex_1,);
+    }
 
     static std::unique_ptr<Graph> createGraph(std::istream& is);
 };
