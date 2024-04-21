53794626b83fc6b6f25b646b06bf39761fd3875d
Wiktor Jankowski Ostrowski <122821918+WiktorPWR@users.noreply.github.com>
2024-04-21 15:19:48 +0200

    Delete Projekt 2 directory

--- Projekt 2/src/adjacency_list_graph.cpp
+++
@@ -1,8 +0,0 @@
-#include "graphs/adjacency_list_graph.hpp"
-
-std::unique_ptr<Graph> AdjacencyListGraph::createGraph(std::istream& is)
-{
-    // TODO: implement
-
-    return std::make_unique<AdjacencyListGraph>();
-}
--- Projekt 2/src/adjacency_matrix_graph.cpp
+++
@@ -1,8 +0,0 @@
-#include "graphs/adjacency_matrix_graph.hpp"
-
-std::unique_ptr<Graph> AdjacencyMatrixGraph::createGraph(std::istream& is)
-{
-    // TODO: implement
-
-    return std::make_unique<AdjacencyMatrixGraph>();
-}
